4f42cfa2946849341bd63b97ae06cf2ccd5e3223
Krishna Gupta <gupta.krishnalko111@gmail.com>
2023-03-10 20:40:15 +0530

    Fixed minor design bugs

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -8,7 +8,7 @@ export default function Blog(props) {
                 <p className="text-2xl md:text-4xl font-semibold">{props.title}</p>
                 <div className="my-2 flex items-center">
                 <Image loading="lazy" alt="photo" className="rounded-full ml-0 mr-2 md:mx-2" src={props.photo} width={30} height={30}/>
-                    <span className="text-orange-700 font-bold mr-2">{props.author} - </span>
+                    <span className="text-orange-700 font-bold text-[15px] mr-2">{props.author} - </span>
                     <span> { props.publishedDate}</span>
                 </div>
             </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,18 +8,18 @@ export default function Hero(){
                 <Image src={img1} alt={"photo"} width={500}/>
             </div>
             <div className="right w-[90%] m-auto md:w-[45%]">
-                <div className="flex items-center">
+                <div className="text-2xl md:text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div className="flex items-center my-2">
                     <div className="flex items-center mr-2">
                     <div>
-                        <Image src={"https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=380&h=400&q=80`"} height={30} alt={"author"} width={55} className="rounded-full mx-4"/>
+                        <Image src={"https://xsgames.co/randomusers/assets/avatars/male/74.jpg"} height={30} alt={"author"} width={55} className="rounded-full mr-2"/>
                     </div>
                     <div className="flex flex-col">
-                        <span className="font-bold text-orange-700">Flying High</span>
+                        <span className="font-bold text-orange-700 text-[14px]">Sam Bunkman</span>
                     </div>
                 </div>
-                    <span> -  Jun 3, 2022</span>
+                    <span className="text-[14px]"> -  Jun 3, 2022</span>
                 </div>
-                <div className="text-2xl md:text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
                 <div className="leading-20 my-3 md:my-6 text-slate-800">
                 The main purpose of letters is the practical one of making thoughts visible. Ruskin says that all letters are frightful things and are to be endured upon occasion that is to say, in places where the inscription is of more importance than external ornament.
                 </div>
